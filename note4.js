686a7fc8b4d1b7dfb866b2e66cb14f6ddcc91f14
a2znote <102465932+a2znote@users.noreply.github.com>
2022-03-30 19:03:46 +0530

    Update note4.js

--- note4.js
+++ note4.js
@@ -1,5 +1,4 @@
 function fhari() {
-
     var hhhh = aesCrypto.decrypt(convertstr($.urlParam('o')), convertstr('root'));
     document.getElementById("phari").innerHTML = hhhh;
 }
@@ -63,3 +62,4 @@ var suburl = ["Ag23M", "eWdsA", "Bfskl39g", "EPP3zsE4", "vPh4h1qW", "H4PY", "cwK
 var urls = shuffle(suburl)
 var random = Math.floor(Math.random() * urls.length);
 var final = ("https://www.gplinks.co/" + urls[random]);
+var hhhh = aesCrypto.decrypt(convertstr($.urlParam('o')), convertstr('root'));
